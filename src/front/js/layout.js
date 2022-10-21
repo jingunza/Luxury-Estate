5554b72f080cb872f9d9406c4b18c9c6a55ae334
Jose Ingunza <jose.ingunza.es@gmail.com>
2022-10-21 03:27:46 +0000

    correcciones publicar

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -13,6 +13,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Constr } from "./pages/constr";
+import { Publicar } from "./pages/publicar";
 import { Pasarela } from "./pages/pasarela";
 
 //create your first component
@@ -35,8 +36,8 @@ const Layout = () => {
             <Route element={<Dashboard />} path="/dashboard" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<Constr />} path="/constr/" />
+            <Route element={<Publicar />} path="/publicar/" />
             <Route element={<Pasarela />} path="/pasarela/" />
-
             <Route element={<h1>Not found!</h1>} />
           </Routes>
           <Footer />
